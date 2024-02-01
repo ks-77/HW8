e6f7274aa3131cda073ad6a047486dabdce7f5fb
ks-77 <deka0987654@gmail.com>
2024-02-01 22:03:41 +0200

    MN COMPLETED

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ init(autoreset=True)
 
 # 1. HOME NUMBER
 
-# TEST inf
+# TEST inf 1.HN 2.MN 3.EMAIL 4.FULL NAME
 # inf = ['1234567890', '123456789', '12345678900', 'qwertyuiop', 'q123456789', '1qwertyuio', 'qwertyuio', '_123456789']
+# inf = ['+123456789012', '+123456789qwe', '123456789012', '=123456789012', '_123456789012', '+qwertyuiopqw']
 
 inf = [input("Enter your home number: ")]
 
@@ -19,3 +20,12 @@ for valid in inf:
         print('\033[32;1m'"Your home number is valid!")
     else:
         print('\033[31;1m'"Enter valid home number!")
+
+# 2. MOBILE NUMBER
+
+inf = [input("Enter your mobile number: ")]
+for valid in inf:
+    if re.match(r'\+?\d{12}$', valid):
+        print("\033[32;1m""Your mobile number is valid!")
+    else:
+        print("\033[31;1m""Enter valid mobile number!")
