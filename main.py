f5cfad363e356218b0b2cb2d267ebe07fd8563a5
ks-77 <deka0987654@gmail.com>
2024-02-01 21:24:20 +0200

    HN COMPLETED

--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 HW8
 Savchenko Kirill
 """
+
+import re
+from colorama import init
+init(autoreset=True)
+
+# 1. HOME NUMBER
+
+# TEST inf
+# inf = ['1234567890', '123456789', '12345678900', 'qwertyuiop', 'q123456789', '1qwertyuio', 'qwertyuio', '_123456789']
+
+inf = [input("Enter your home number: ")]
+
+for valid in inf:
+    if re.match(r'\d{10}$', valid):
+        print('\033[32;1m'"Your home number is valid!")
+    else:
+        print('\033[31;1m'"Enter valid home number!")
