4c0fd724429937029e97e931963fb26e29a114c0
ks-77 <deka0987654@gmail.com>
2024-02-02 02:08:45 +0200

    FULL NAME COMPLETED

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ init(autoreset=True)
 # inf = ['1234567890', '123456789', '12345678900', 'qwertyuiop', 'q123456789', '1qwertyuio', 'qwertyuio', '_123456789']
 # inf = ['+123456789012', '+123456789qwe', '123456789012', '=123456789012', '_123456789012', '+qwertyuiopqw']
 # inf = ['dd@dd.dd', 'dddd.dd', '23de@ww3ww', '12232323', 'srfhg@srf.hg.com', 'wrt@y.uio', 'wrt@yu@io.cc', '23.23@23']
+# inf = ['wht rdg erg', '234 235 252', 'fgh4 rt 45', 'fgh. ggg rtg', 'fgh t ttt']
 
 inf = [input("Enter your home number: ")]
 
 for valid in inf:
-    if re.match(r'\d{10}$', valid):
+    if re.match(r'^\d{10}$', valid):
         print('\033[32;1m'"Your home number is valid!")
     else:
         print('\033[31;1m'"Enter valid home number!")
@@ -26,7 +27,7 @@ for valid in inf:
 
 inf = [input("Enter your mobile number: ")]
 for valid in inf:
-    if re.match(r'\+?\d{12}$', valid):
+    if re.match(r'^\+?\d{12}$', valid):
         print("\033[32;1m""Your mobile number is valid!")
     else:
         print("\033[31;1m""Enter valid mobile number!")
@@ -35,7 +36,16 @@ for valid in inf:
 
 inf = [input("Enter your email: ")]
 for valid in inf:
-    if re.match(r'\w{2,64}@\w{2,127}\.\w{2,127}', valid):
+    if re.match(r'^\w{2,64}@\w{2,127}\.\w{2,127}$', valid):
         print("\033[32;1m""Your email is valid!")
     else:
         print("\033[31;1m""Enter valid email!")
+
+# 4. FULL NAME
+
+inf = [input("Enter your full name: ")]
+for valid in inf:
+    if re.match(r'^[A-Za-z]{2,20}\s[A-Za-z]{2,20}\s[A-Za-z]{2,20}$', valid):
+        print("\033[32;1m""Your full name is valid!")
+    else:
+        print("\033[31;1m""Enter valid full name!")
